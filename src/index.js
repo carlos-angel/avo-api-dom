7f65df4ab13a3b1505805f695c261376c8acbb22
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-11-20 22:01:27 -0600

    feat: crear los nodos para mostrar los items de avocados

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const URL = 'https://platzi-avo.vercel.app/api/avo';
 
-console.log('Happy hacking :)')
+window
+  .fetch(URL)
+  .then((resp) => resp.json())
+  .then((respJson) => {
+    const items = [];
+    respJson.data.forEach((item) => {
+      const image = document.createElement('img');
+      const title = document.createElement('h2');
+      const price = document.createElement('div');
+
+      const container = document.createElement('div');
+
+      container.append(image, title, price);
+      items.push(container);
+    });
+
+    document.body.append(...items);
+  });
