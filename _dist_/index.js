76f63ec2b0a4894344acb24c5f7517308f046278
carlos-angel <carlos-angel@users.noreply.github.com>
2022-11-23 14:51:34 +0000

    deploy: edf1756fc9c46c8f1458f572a8ed8593e269cf99

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,6 +1,46 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const URL = 'https://platzi-avo.vercel.app/api/avo';
 
-console.log('Happy hacking :)')
+const DOMAIN = 'https://platzi-avo.vercel.app';
+
+function formatPrice(price) {
+  const newPrice = window.Intl.NumberFormat('en-EN', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(price);
+  return newPrice;
+}
+
+const app = document.querySelector('div#app');
+app.className = 'mt-10 grid grid-cols-2 gap-2';
+
+window
+  .fetch(URL)
+  .then((resp) => resp.json())
+  .then((respJson) => {
+    const items = [];
+    respJson.data.forEach((item) => {
+      const image = document.createElement('img');
+      image.src = `${DOMAIN}/${item.image}`;
+      image.className = 'h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6';
+
+      const title = document.createElement('h2');
+      title.textContent = item.name;
+      title.className = 'text-lg';
+
+      const price = document.createElement('div');
+      price.textContent = formatPrice(item.price);
+      price.className = 'text-gray-600';
+
+      const priceAndTitle = document.createElement('div');
+      priceAndTitle.className = 'text-center md:text-left';
+      priceAndTitle.append(title, price);
+
+      const container = document.createElement('div');
+      container.className = 'md:flex bg-white rounded-lg p-6 hover:bg-gray-300';
+
+      container.append(image, priceAndTitle);
+      items.push(container);
+    });
+
+    app.append(...items);
+  });
