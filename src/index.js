edf1756fc9c46c8f1458f572a8ed8593e269cf99
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-11-23 08:50:51 -0600

    feat: implementar la API de internacionalización para el precio

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ const URL = 'https://platzi-avo.vercel.app/api/avo';
 
 const DOMAIN = 'https://platzi-avo.vercel.app';
 
+function formatPrice(price) {
+  const newPrice = window.Intl.NumberFormat('en-EN', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(price);
+  return newPrice;
+}
+
 const app = document.querySelector('div#app');
 app.className = 'mt-10 grid grid-cols-2 gap-2';
 
@@ -20,7 +28,7 @@ window
       title.className = 'text-lg';
 
       const price = document.createElement('div');
-      price.textContent = `$${item.price}`;
+      price.textContent = formatPrice(item.price);
       price.className = 'text-gray-600';
 
       const priceAndTitle = document.createElement('div');
